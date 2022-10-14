7869435021e3556844f599d2433685669c5d0ca9
jvmoreirad <jvitormoreirad@hotmail.com>
2022-10-13 22:32:52 -0300

    fix: correction on checkbox props

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,13 +1,14 @@
 import * as CheckboxRadix from "@radix-ui/react-checkbox"
 import { Check } from "phosphor-react"
-export interface ICheckBoxProps {
+export interface ICheckBoxProps extends CheckboxRadix.CheckboxProps {
 
 }
 
-export function CheckBox({}: ICheckBoxProps) {
+export function CheckBox(props: ICheckBoxProps) {
     return (
         <CheckboxRadix.Root
             className="w-6 h-6 p-0.5 bg-gray-800 rounded"
+            {...props}
         >
             <CheckboxRadix.CheckboxIndicator asChild>
                 <Check weight="bold" className="h-5 w-5 text-cyan-500" />
